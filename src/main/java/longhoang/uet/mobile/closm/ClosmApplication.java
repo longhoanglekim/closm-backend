c5f339b0fe641e47f7e71e8c79d38193ea5b6237
longhoanglekim <longngerr@gmail.com>
2025-02-28 13:53:06 +0700

    init project

---
+++ src/main/java/longhoang/uet/mobile/closm/ClosmApplication.java
@@ -0,0 +1,13 @@
+package longhoang.uet.mobile.closm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClosmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/longhoang/uet/mobile/closm/ClosmApplicationTests.java
@@ -0,0 +1,13 @@
+package longhoang.uet.mobile.closm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClosmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
