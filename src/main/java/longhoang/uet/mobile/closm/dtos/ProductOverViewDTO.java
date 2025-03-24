a1df802e57aea3867280e51bba430b0979ae5484
ntdungg15 <ngtiendung0105204@gmail.com>
2025-03-24 17:57:21 +0700

    save

--- src/main/java/longhoang/uet/mobile/closm/dtos/ProductOverViewDTO.java
+++ src/main/java/longhoang/uet/mobile/closm/dtos/ProductOverViewDTO.java
@@ -1,4 +1,18 @@
 package longhoang.uet.mobile.closm.dtos;
 
+
 public class ProductOverviewDTO {
+    private String name;
+    private double price;
+    private String category;
+    private String image;
+    private double rating;
+
+    public ProductOverviewDTO(String name, double price, String category, String image, double rating) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.image = image;
+        this.rating = rating;
+    }
 }
